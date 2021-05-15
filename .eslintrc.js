e8cee14bc0a21ba747e28582fab02bea596ee0d9
Víctor Cruz <vmcruz16@gmail.com>
2021-05-15 12:47:42 -0500

    Update packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,16 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "env": {
-      "browser": true,
-    },
-    "rules": {
-      "no-param-reassign": ["error", { "props": false }],
-    },
+  extends: 'airbnb-base',
+  env: {
+    browser: true,
+    jasmine: true,
+  },
+  rules: {
+    'no-param-reassign': ['error', { props: false }],
+  },
+  globals: {
+    UnitJS: true,
+    $: true,
+    requiredError: true,
+    FIXTURES: true,
+  },
 };
